410ef1fbddc47258a7e95cbf1809145531a1bd7c
akiogitgit <akimaron2828@icloud.com>
2022-09-26 08:55:02 +0900

    feat: semantic ui を導入

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,10 +4,48 @@ import { onMounted } from 'vue'
 onMounted(() => {
   initTheme()
 })
+const myName = 'anpan'
+const myClass = 'text-red'
 </script>
 
 <template>
   <h1>index</h1>
+  <div>私の名前は{{ myName + 3 }}です</div>
+  <div class="{{myClass}}">NG {myClass}</div>
+  <div :class="myClass">OK text-red</div>
+  <div :class="[myClass, myClass]">OK text-red text-red</div>
+  <div :class="{ myClass }">NG myClass</div>
+  <div :class="{ green: true, red: false }">green</div>
+  <div :class="{ green: 5 > 1, red: false && 9 < 16 }">green</div>
+  <button class="ui button">Button</button>
+  <!-- 固定値と、変動値のクラス -->
+  <div
+    class="text-red text-blue"
+    :class="{ green: 5 > 1, red: false && 9 < 16 }"
+  >
+    green
+  </div>
+  <div
+    :class="{
+      'text-red': true,
+      'text-blue': true,
+      green: 5 > 1,
+      red: false && 9 < 16,
+    }"
+  >
+    green
+  </div>
+
+  <!-- <button :onClick="{() => alert(`alert`)}">alert</button> -->
+  <button v-on:click="() => alert(`alert`)">alert</button>
+  <button v-on:click="() => alert('こんにちは！')"></button>
+  <!-- <button @click="() => alert('こんにちは！')">alert</button> -->
+  <button v-on:click="counter += 1">Add 1</button>
 </template>
 
-<style></style>
+<style>
+.text-red {
+  color: 'red';
+  background-color: 'red';
+}
+</style>
