1b78cc59263f5ce2c16252052f6ae558d83125b2
akiogitgit <akimaron2828@icloud.com>
2022-09-26 16:25:51 +0900

    feat: todoList作成

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import Index from './pages/index.vue'
 import Test from './pages/test.vue'
 import Lesson1 from './pages/lesson1.vue'
+import Lesson2 from './pages/lesson2.vue'
 
 export const routes = [
   { path: '/', component: Index },
   { path: '/test', component: Test },
   { path: '/lesson1', component: Lesson1 },
+  { path: '/lesson2', component: Lesson2 },
 ]
 
 export default routes
